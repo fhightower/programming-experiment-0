d8da18a5722e6909ae3b7bb0e7c43d11b94580c1
Floyd Hightower <floyd.hightower27@gmail.com>
2022-08-05 05:37:13 -0400

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM python:3.10.2-buster
+
+ENV PIP_NO_CACHE_DIR "true"
+
+COPY ./requirements*.txt /code/
+
+WORKDIR /code
+
+RUN pip install -r requirements_dev.txt
+
---
+++ starting-point.py
@@ -0,0 +1,19 @@
+# The goal is to create a mine-sweeper board with the given number of cols, rows, and mines
+# The board should use "x" for mines and each square should contain the number of adjacent mines
+# (consider squares in both cardinal and ordinal directions)
+
+# For example, a 3x3 board with 3 mines should look something like:
+# 1 2 2
+# 1 x x
+# 1 3 x
+
+
+def create_board(cols, rows, mines):
+    pass
+
+
+create_board(3, 3, 3)
+create_board(1, 10, 1)
+create_board(11, 2, 1)
+create_board(11, 2, 11)
+
